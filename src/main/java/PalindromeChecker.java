5fb1508d25e982ac453293287e96ecf68af40e93
chwadley <chwadley@s.sfusd.edu>
2025-01-27 11:19:50 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,15 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
-  return false;
+  String rev = reverse(word);
+  return rev.equals(word);
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+    for (int i=str.length-1;i>=0;i++) {
+      sNew+=str.substring(i,i+1);
+    }
     return sNew;
 }
 }
