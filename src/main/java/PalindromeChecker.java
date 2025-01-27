61bf9bbe78296aa7b6cfad8699e4af576d8f0f31
chwadley <chwadley@s.sfusd.edu>
2025-01-27 11:22:07 -0800

    change str.length to str.length()

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -43,7 +43,7 @@ public boolean palindrome(String word)
 public String reverse(String str)
 {
     String sNew = new String();
-    for (int i=str.length-1;i>=0;i++) {
+    for (int i=str.length()-1;i>=0;i++) {
       sNew+=str.substring(i,i+1);
     }
     return sNew;
